ed0b5ca9c0e640b29029db447fae9fac9e2e02b0
Collin Broderick <sedrahedron@gmail.com>
2020-09-27 23:14:52 -0400

    Amended playGame function

--- AI_Project2.py
+++ AI_Project2.py
@@ -6,5 +6,12 @@ def main():
     playGame()
 
 def playGame():
-    while not path.exists(groupName+".txt") #Waits until it is the player's move
-    #Make move here
+    while not path.exists(groupName+".go"): #waits until it is the player's move
+       pass
+    if path.exists("endgame"):
+        #end of game
+        print("End of game")
+    else:
+        #read opponent's move here
+        #make move here
+        playGame() #repeats until game completion
