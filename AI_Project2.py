8bfedc6e46abfb87bbc56e9028a78a12ad495170
Collin Broderick <sedrahedron@gmail.com>
2020-09-27 22:33:08 -0400

    Added playGame function

--- AI_Project2.py
+++ AI_Project2.py
@@ -1,1 +1,10 @@
+import os.path
+from os import path
+groupName = "Sigmoid"
 
+def main():
+    playGame()
+
+def playGame():
+    while not path.exists(groupName+".txt") #Waits until it is the player's move
+    #Make move here
