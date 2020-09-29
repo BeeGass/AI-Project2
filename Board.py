604b31a41d2372bad4187535d7a336f2d9899a74
Collin Broderick <sedrahedron@gmail.com>
2020-09-29 00:44:59 -0400

    Added BoardConfiguration class and added board field to Move

--- Board.py
+++ Board.py
@@ -1,35 +1,39 @@
-class Board:
+#Class representing a possible board state
+class BoardConfiguration:
     boardSize = 15
-    board: list
+    boardList: list
+    def __init__(self):
+        self.boardList = [[0 for x in range(self.boardSize)] for y in range(self.boardSize)]
+
+#Represents the current board state of the game
+class Board:
+    boardConfiguration : BoardConfiguration
     selfMoves = [] #list representing moves made by the AI player (self)
     oppMoves = [] #list representing moves made by the opposing player
     def __init__(self):
-        self.board = [[0 for x in range(self.boardSize)] for y in range(self.boardSize)]
-        #initializes a 15x15 board
-        #0 represents an empty space, 1 represents a space occupied by the AI player, and 2 represents a space occupied by the
-        #opposing player
+        self.boardConfiguration = BoardConfiguration()
 
     #Places a piece on the board
     #row: The row
     #col: The column
     #Player: 1 if AI player, 2 if opponent
     def placePiece(self, row, col, utility, player):
-        #global selfMoves
-        #global board
-        self.board[row][col] = player
+        self.boardConfiguration.boardList[row][col] = player
         if player == 1:
-            self.selfMoves.append(Move(player, row, col, 0))
+            self.selfMoves.append(Move(player, row, col, utility, self.boardConfiguration))
         if player == 2:
-            self.oppMoves.append(Move(player, row, col, 0))
+            self.oppMoves.append(Move(player, row, col, utility, self.boardConfiguration))
 
 class Move: #class representing a move made by either player
     player: int #the player that made the move
     row: int #the row the piece was placed in
     col: int #the column the piece was placed in
     utility: float #the utility represented by the move
+    board : BoardConfiguration #the board configuration after the move has been made
 
-    def __init__(self, player, row, col, utility):
+    def __init__(self, player, row, col, utility, board):
         self.player = player
         self.row = row
         self.col = col
         self.utility = utility
+        self.board = board
