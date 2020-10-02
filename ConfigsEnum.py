5b5088cc0ccc1e21ad5b730932455c2dc8241d4c
cbroderick-PC\cbroderick <sedrahedron@gmail.com>
2020-10-01 22:51:29 -0400

    Fixed up tentative CalcPathUtil function

--- ConfigsEnum.py
+++ ConfigsEnum.py
@@ -1,6 +1,10 @@
 from enum import Enum
 
-class Configs (Enum):
+class ConfigsEnum (Enum):
     FIVE = 100
-    LIVEFOUR = 50
-    DEADFOUR = 45
+    LIVEFOUR = 15
+    LIVETHREE = 10
+    LIVETWO = 5
+    DEADFOUR = 10
+    DEADTHREE = 5
+    DEADTWO = 1
