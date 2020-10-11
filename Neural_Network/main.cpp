92984e080d288f05627f0b3d413ff19476555526
Bryan <44324535+BeeGassy@users.noreply.github.com>
2020-10-11 16:52:01 -0400

    Create main.cpp

---
+++ Neural_Network/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+
+    vector<double> inputVals;
+
+    void ActivationFunction();
+    void FeedForward(const vector<double> &inputVals);
+    void Backpropagation(inputTargetVals);
+    void GetResults(inputResultVals);
+    void LinearRegression();
+    void AdjustWeights();
+    void Summation();
+    void CalculateError();
+    void CalculateLossTwo();
+    void GradientDescent();
+    void ManipulateData();
+
+    return 0;
+}
