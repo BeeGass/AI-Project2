7626881b573dc309ee01f347fd2b83f0e91093da
Bryan <44324535+BeeGassy@users.noreply.github.com>
2020-10-11 16:52:17 -0400

    Update NeuralNetwork.h

--- Neural_Network/NeuralNetwork.h
+++ Neural_Network/NeuralNetwork.h
@@ -1,6 +1,13 @@
-class Net {
+#include vector
+
+class NeuralNetwork {
 
 public:
+    NeuralNetwork(inputTopology);
+    void ActivationFunction(inputVals);
+    void FeedForward(inputVals);
+    void Backpropagation(inputTargetVals);
+    void GetResults(inputResultVals) const;
 
 private:
 
