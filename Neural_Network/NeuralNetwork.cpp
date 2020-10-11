05a28b47a11f2d27e67065c8e66532443c786855
Bryan <44324535+BeeGassy@users.noreply.github.com>
2020-10-11 16:52:40 -0400

    Update NeuralNetwork.cpp

--- Neural_Network/NeuralNetwork.cpp
+++ Neural_Network/NeuralNetwork.cpp
@@ -1,1 +1,45 @@
 #include NeuralNetwork.h
+
+void ActivationFunction(){
+
+}
+
+void FeedForward(inputVals){
+
+}
+
+void Backpropagation(inputTargetVals){
+
+}
+
+void GetResults(inputResultVals){
+
+}
+
+void LinearRegression(){
+
+}
+
+void AdjustWeights(){
+
+}
+
+void Summation(){
+
+}
+
+void CalculateError(){
+
+}
+
+void CalculateLossTwo(){
+
+}
+
+void GradientDescent(){
+
+}
+
+void ManipulateData(){
+
+}
